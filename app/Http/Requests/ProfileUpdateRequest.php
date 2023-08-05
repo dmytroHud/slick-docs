8aaef3eac1fee474ed2044f1f0d3b9c6c81d3ef3
dmitriyGud <dmitriy.gudenko@dreamdev.email>
2023-08-05 20:36:09 +1000

    finished author avatar feature

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,7 +18,7 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'profileImage' => ['max:1024']
+            'avatar' => ['mimes:jpeg,png,jpg|max:1024']
         ];
     }
 }
